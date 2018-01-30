d93b3857f1153cdf66011c45b9f8095a2f83e25f
Diloan Silva <diloan.silva@gmail.com>
2018-01-30 10:10:05 -0300

    Garbage Collector (Coletor de Lixo)

---
+++ estudos/src/estudo/java/detran/loiane66/ColetorLixo.java
@@ -0,0 +1,40 @@
+package estudo.java.detran.loiane66;
+
+import estudo.java.detran.loiane61.Contato;
+
+public class ColetorLixo {
+
+	
+	public static void obterMemoriaUsada(){
+		
+		final int MB = 1024 * 1024;
+		
+		Runtime runtime =  Runtime.getRuntime(); // Padrão Singleton
+		
+		System.out.println((runtime.totalMemory() - runtime.freeMemory()) / MB);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Contato[] contatos = new Contato[1000000];
+		Contato contato;
+		
+		for(int i = 0; i < contatos.length; i++){
+			contato = new Contato("Contato" + i, "1234-5678" + i, "contato"+i+"@gmail.com");
+			contatos[i] = contato;
+		}
+		
+		System.out.println("Contatos criados");
+		obterMemoriaUsada();
+		
+		contatos = null;
+		Runtime.getRuntime().runFinalization();
+		Runtime.getRuntime().gc();
+		
+		System.out.println("Contatos removidos da memória");
+		obterMemoriaUsada();
+	
+	}
+
+}
