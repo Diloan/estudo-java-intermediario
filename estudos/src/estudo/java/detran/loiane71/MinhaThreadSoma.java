90be61c51edb8cf78fe137bfb565486840f93843
Diloan Silva <diloan.silva@gmail.com>
2018-02-15 09:30:32 -0300

    métodos e blocos sincronizados (synchronized)

---
+++ estudos/src/estudo/java/detran/loiane71/MinhaThreadSoma.java
@@ -0,0 +1,31 @@
+package estudo.java.detran.loiane71;
+
+public class MinhaThreadSoma implements Runnable{
+	
+	private String nome;
+	private int[] nums;
+	private static Calculadora calc = new Calculadora();
+		
+	public MinhaThreadSoma(String mome, int[] nums){
+		this.nome = mome;
+		this.nums = nums;
+		new Thread(this, nome).start();
+//		Thread t = new Thread(this, nome);
+//		t.start();
+	}
+
+	@Override
+	public void run() {
+		
+		System.out.println(this.nome + " iniciada");
+		
+		int soma = calc.somaArray(this.nums);
+		
+		System.out.println("Resultado da soma para thread " + this.nome + " é: " + soma);
+		
+		System.out.println(this.nome + " terminada");
+	}
+	
+	
+	
+}
